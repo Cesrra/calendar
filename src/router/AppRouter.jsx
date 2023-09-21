c16de519d694811b222174fa4a98e2f4199655c9
cesrra <c3z4r.rincon@gmail.com>
2023-09-21 00:08:12 -0500

    Add the pricnipals components like Login and Calendar. Add ther Router and the two path to navigate

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Login } from "../components/auth"
+import { Calendar } from "../components/calendar"
+
+export const AppRouter = () => {
+  
+  const authStatus = "notAuthenticadte"
+
+  return (
+    <Routes>
+      {
+        (authStatus === "notAuthenticate") 
+          ? <Route path="/auth/*" element={ <Login /> } />
+          : <Route path="/*" element={ <Calendar /> } />
+      }
+      
+      <Route path="/*" element={ <Navigate to="/auth/login" /> } />
+    </Routes>
+  )
+}
