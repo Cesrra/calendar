6f6f6de367dea4a2a8718d4dec7d088ad4444b02
cesrra <c3z4r.rincon@gmail.com>
2023-09-21 01:20:24 -0500

    Add the lybreires to Calendar, date-fns, and stayles

--- src/components/calendar/pages/Calendar.jsx
+++ src/components/calendar/pages/Calendar.jsx
@@ -1,7 +1,47 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { Navbar } from "../../"
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
 
-export const Calendar = () => {
+import es from 'date-fns/locale/es'
+
+const locales = {
+  'es': es
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const eventsList = [
+  {
+    title: "Cesar Date",
+    notes: "Need go to the reataurant",
+    start: new Date(),
+    end: addHours( new Date(), 2 ),
+    bgColor: "#6610f2",
+    user: {
+      _id: "123",
+      name: "Cesar"
+    }
+  }
+]
+
+export const CalendarPage = () => {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Calendar 
+        localizer={localizer}
+        events={eventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px)' }}
+        />
+    </>
   )
 }
